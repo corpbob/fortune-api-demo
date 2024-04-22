f2264fab7ceef5fc09fb3b4994682fef813b5b91
corpbob <me@example.com>
2024-04-22 19:53:24 +0800

    use async call

--- routes/fortune.js
+++ routes/fortune.js
@@ -6,8 +6,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 //  var fortune = child_process.execSync('fortune');
 //  res.send(fortune.toString() + '\n');
-console.log("executing command: " + command);
-child_process.exec(command, (error, stdout, stderr) => {
+//console.log("executing command: " + command);
+child_process.exec('fortune', (error, stdout, stderr) => {
   if (error) {
     console.error("Error executing command:", error);
     res.status(500).send("Error executing command");
