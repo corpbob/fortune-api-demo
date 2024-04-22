de6c4e73a0ab5dd532cd553fff834f5bf7e081e8
corpbob <me@example.com>
2024-04-22 20:18:54 +0800

    revert

--- routes/fortune.js
+++ routes/fortune.js
@@ -4,17 +4,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-//  var fortune = child_process.execSync('fortune');
-//  res.send(fortune.toString() + '\n');
+  var fortune = child_process.execSync('fortune');
+  res.send(fortune.toString() + '\n');
 //console.log("executing command: " + command);
-child_process.exec('fortune', (error, stdout, stderr) => {
-  if (error) {
-    console.error("Error executing command:", error);
-    res.status(500).send("Error executing command");
-    return;
-  }
-  res.send(stdout + '\n');
-});
+//child_process.exec('fortune', (error, stdout, stderr) => {
+//  if (error) {
+//    console.error("Error executing command:", error);
+//    res.status(500).send("Error executing command");
+//    return;
+//  }
+//  res.send(stdout + '\n');
+//});
 
 });
 
